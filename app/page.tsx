578a65696ada8f7306df511a5fc330adfb333625
Matheus Santana <santalvesmatheus@gmail.com>
2024-02-06 20:52:26 +0000

    chore: create initial routes

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Nextjs 14 we&apos;re comming!</h1>
-      <h2 className="h2-bold">Nextjs 14 we&apos;re comming!</h2>
-    </div>
-  );
-}
